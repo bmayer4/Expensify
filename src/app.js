e2315bfe85fa52501dcea77e3b6cd23ef32fd6ef
bmayer4 <bmayer55@yahoo.com>
2017-12-02 16:26:45 -0500

    removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -11,19 +11,6 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';  //this was in ExpenseForm but we moved it
 
 const store = configureStore();
-console.log(store.getState());
-
-//called any time an action is dispatched
-store.subscribe(() => {
-    const state = store.getState();
-    const visibileExpenses = getVisibleExpenses(state.expenses, state.filters);
-    console.log('from subscribe in app.js', visibileExpenses);
-
-});
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 400 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 600, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 
 const jsx = (
     <Provider store={store}>

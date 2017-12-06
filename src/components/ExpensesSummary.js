6b5c3bfae9c552084968113f9ecca665c634fb57
bmayer4 <bmayer55@yahoo.com>
2017-12-05 19:40:00 -0500

    changes to account summary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -6,7 +6,7 @@ import numeral from 'numeral';
 
 //export here for test
 export const ExpensesSummary = ({expenseCount, expensesTotal}) => {
-    const expenseWord =  expenseCount === 1 ? 'expenses' : 'expense'
+    const expenseWord =  expenseCount === 1 ? 'expense' : 'expenses'
     const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
     return (
         <div>

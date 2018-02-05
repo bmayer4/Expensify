fc18fc53582fe809b3dfe367544ba3b9ff04c649
bmayer4 <bmayer55@yahoo.com>
2018-02-04 21:01:48 -0500

    app complete

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,48 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux'; //connects your component to the redux store
 import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from './../selectors/expenses';
 
 //exported below as named export for testing purposes, even though default export is used by app
-class ExpenseList extends Component {
-
-    state = {
-        limit: 10
-    }
-
-    showMore = () => {
-        this.setState((prevState) => ({ limit: prevState.limit + 10 }));
-    };
-
-  render() {
-    return (
-      <div className="content-container">
-        <div className="list-header">
-          <div className="show-for-mobile">Expenses</div>
-          <div className="show-for-desktop">Expense</div>
-          <div className="show-for-desktop">Amount</div>
+const ExpenseList = (props) => (
+    <div className="content-container">
+    <div className="list-header">
+        <div className="show-for-mobile">Expenses</div>
+        <div className="show-for-desktop">Expense</div>
+        <div className="show-for-desktop">Amount</div>
+    </div>
+    <div className="list-body">
+        {props.expenses.length === 0 ? (
+        <div className="list-item list-item--message">
+            <span>No Expenses</span>
         </div>
-        <div className="list-body">
-          {this.props.expenses.length === 0 ? (
-            <div className="list-item list-item--message">
-              <span>No Expenses</span>
-            </div>
-          ) : (
-            this.props.expenses.slice(0, this.state.limit).map(expense => {
-              return (
-                <ExpenseListItem
-                  key={expense.id}
-                  {...expense} //spreads out all the properties on expense, as props  ex: id={4344}, description={'rara'}
-                />
-              );
-            })
-          )}
-        </div>
-        {this.props.expenses.length > this.state.limit ? <button className="show-more" onClick={this.showMore}>Show More</button> : null}
-      </div>
-    );
-  }
-}
+        ) : (
+        props.expenses.map(expense => {
+            return (
+            <ExpenseListItem
+                key={expense.id}
+                {...expense} //spreads out all the properties on expense, as props  ex: id={4344}, description={'rara'}
+            />
+            );
+        })
+        )}
+    </div>
+    </div>
+);
 
 const mapStateToProps = state => {
   return {
